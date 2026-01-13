7f3bee5feb0f4f906908b88a53d379942c16bbe2
Amazing Rofa <arofa2008@gmail.com>
2026-01-13 15:24:14 +0400

    Add project files.

---
+++ WebApplication2/Pages/Custom/2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages.Custom
+{
+    public class _2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
